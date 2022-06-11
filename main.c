37a4a92c2a60e6030fbc06fcb3afddab7698b9f8
snowdy <carmine.marchesani@studenti.unicam.it>
2022-06-11 19:14:42 +0200

    [update] defined methods in op.h and printf in main.c

--- main.c
+++ main.c
@@ -1,1 +1,8 @@
-
+#include "op.h"
+#include <stdio.h>
+int main()
+{
+    printf("The sum is: %d", sum(10,5));
+    printf("\nThe difference is: %d", difference(40,35));
+    return 0;
+}
--- op.h
+++ op.h
@@ -1,4 +1,28 @@
 #ifndef C_LIBRARY_CREATION_AND_USAGE_OP_H
 #define C_LIBRARY_CREATION_AND_USAGE_OP_H
+/**
+ * This method calculates the sum of the two given arguments
+ * @param a
+ * @param b
+ * @return the sum of the two given arguments
+ */
 
+ int sum(int a, int b)
+{
+     double result = a + b;
+     return result;
+}
+
+/**
+ * This method calculates the difference of the two given arguments
+ * @param a
+ * @param b
+ * @return the difference of the two given arguments
+ */
+
+int difference(int a, int b)
+{
+    double result = a - b;
+    return result;
+}
 #endif //C_LIBRARY_CREATION_AND_USAGE_OP_H
